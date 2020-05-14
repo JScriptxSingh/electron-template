9065bdc1237725ebef62ac4c0ee42774dbcb672b
jb <jassbhamra@jbs-Mac-mini.local>
2020-05-13 22:24:43 -0700

    all mods done except one

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const electron = require('electron')
+const BrowserWindow = electron.BrowserWindow
+const app = electron.app
 const path = require('path')
+const ipc = electron.ipcMain
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
     }
   })
 
